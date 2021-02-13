36580df96b82d4d8d3d0f1e3d70473ac23f146f9
Rodrigo <ro.gemaque16@gmail.com>
2021-02-13 15:02:43 -0300

    tests array

---
+++ spec/matchers/arrays/array_spec.rb
@@ -0,0 +1,20 @@
+describe 'array formats' do
+  context Array.new([1,2,3]) do
+    it '#include' do #funciona so para elementos do array
+      expect(subject).to include(2)
+      expect(subject).to include(1)  
+    end
+
+    it '#contain_exactly' do # da erro passando os valores como array
+      expect(subject).to contain_exactly(1,2,3)  
+    end
+
+    it '#match_array' do
+      expect(subject).to match_array([1,2,3])  
+    end
+
+
+  end
+  
+
+end
